f3567c68b7fcff4617c041a31881baec7563dba1
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-06-05 11:13:46 +0900

    [#1] chore: application-local.yml

--- src/main/java/sanyunpark99/dessertspot/DessertSpotApplication.java
+++ src/main/java/sanyunpark99/dessertspot/DessertSpotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DessertSpotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DessertSpotApplication.class, args);
     }
-
 }
