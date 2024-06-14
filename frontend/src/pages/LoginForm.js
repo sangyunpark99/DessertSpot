88659bb9bb2ebb3d5a017797fe2b9c8769a99220
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-06-14 10:22:39 +0900

    [#3] feat: LoginForm 구현

---
+++ frontend/src/pages/LoginForm.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../store/authSlice';
+
+function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const { error } = useSelector((state) => state.auth);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(login({ username, password }));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Username</label>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+            </div>
+            <div>
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+            <button type="submit">Login</button>
+            {error && <p>{error}</p>}
+        </form>
+    );
+}
+
+export default LoginForm;
