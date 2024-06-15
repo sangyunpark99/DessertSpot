7a907fed3d331962b253f0f74e527e46d295be34
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-06-15 13:24:38 +0900

    [#1] chore: GlobalExceptionHanlder 구현

---
+++ backend/src/main/java/sanyunpark99/dessertspot/ExceptionController.java
@@ -0,0 +1,16 @@
+package sanyunpark99.dessertspot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ExceptionController {
+
+    @GetMapping("/hello")
+    public void error(){
+        log.info("에러 발생");
+        throw new IllegalStateException("에러 테스트 입니다.");
+    }
+}
