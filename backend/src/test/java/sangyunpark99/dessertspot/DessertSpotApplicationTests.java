f254822c778fff2bd879a3de2c9e5102c1437718
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-03 11:16:09 +0900

    [#9] fix: contextLoads 테스트 코드 Bean creating 오류 해결

--- backend/src/test/java/sangyunpark99/dessertspot/DessertSpotApplicationTests.java
+++ backend/src/test/java/sangyunpark99/dessertspot/DessertSpotApplicationTests.java
@@ -2,9 +2,14 @@ package sangyunpark99.dessertspot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import sangyunpark99.dessertspot.jwt.JwtTokenProvider;
 
 @SpringBootTest
 class DessertSpotApplicationTests {
+
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
 
     @Test
     void contextLoads() {
